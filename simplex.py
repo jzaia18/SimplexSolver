e9d968ad4590bd64e4a8f62ce741c71980d9babb
Jake Zaia <jakezaia00@gmail.com>
2022-10-01 15:17:01 -0400

    Convert input matrix into tableau

--- simplex.py
+++ simplex.py
@@ -1,24 +1,36 @@
-input_matrix = eval(input("Enter your matrix:\n"))
+from fractions import Fraction
 
-matrix_rows = len(input_matrix)
-matrix_cols = len(input_matrix[0])
-
-# First, normalize matrix
-smallest = min([min(row) for row in input_matrix])
-increment = 0
-
-if smallest <= 0:
-    increment = -smallest + 1
-
-# Apply normalization onto matrix
-input_matrix = [[elem + increment for elem in row] for row in input_matrix]
-
-# Extend rows and cols to make tableau shape
-tab = [row + [0]*(matrix_rows) + [1] for row in input_matrix]
-tab.append([-1]*matrix_cols + [0]*(matrix_rows+1))
-
-
+# Print a tableau in a neat format
 def print_tableau(tab):
     print(*tab, sep='\n')
 
-print_tableau(tab)
+
+
+if __name__ == '__main__':
+    input_matrix = eval(input("Enter your matrix:\n"))
+
+    matrix_rows = len(input_matrix)
+    matrix_cols = len(input_matrix[0])
+
+    # First, normalize matrix
+    smallest = min([min(row) for row in input_matrix])
+    increment = 0
+
+    if smallest <= 0:
+        increment = -smallest + 1
+
+        # Apply normalization onto matrix
+        input_matrix = [[elem + increment for elem in row] for row in input_matrix]
+
+    # Extend rows and cols to make tableau shape
+    tab = [row + [0]*(matrix_rows) + [1] for row in input_matrix]
+    tab.append([-1]*matrix_cols + [0]*(matrix_rows+1))
+
+    # Fill in identity matrix
+    for offset in range(matrix_rows):
+        tab[offset][matrix_cols+offset] = 1
+
+    # Convert all numbers to fractions for ease of computation
+    tab = [[Fraction(elem) for elem in row] for row in input_matrix]
+
+    print_tableau(tab)
